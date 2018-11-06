bbce8f21dd75f054c68b53fadabd976c03170a50
Nate Hall <nate.hall@kroger.com>
2018-11-06 14:00:52 -0500

    Fixes broken test

--- src/test/groovy/com/example/CustomListenerSpec.groovy
+++ src/test/groovy/com/example/CustomListenerSpec.groovy
@@ -3,19 +3,27 @@ package com.example
 import org.junit.ClassRule
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.kafka.test.rule.KafkaEmbedded
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
 import spock.lang.Shared
+import spock.util.concurrent.BlockingVariable
 
 @DirtiesContext
-@SpringBootTest(classes = [KafkaIntTestApplication.class])
+@ActiveProfiles('test')
+@SpringBootTest
+// Using @SpringBootTest wires up the whole context and @TestConfiguration just sits on top and doesn't replace the
+// actual configuration so the MockConfig class has to be imported like a real config class.
+@Import(MockConfig.class)
 class CustomListenerSpec extends TestSpecBase {
+
     @ClassRule
     @Shared
-    private KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, 'my-topic')
+    private KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, false, TOPIC_NAME)
 
     @Autowired
     private KafkaTemplate<String, String> template
@@ -27,17 +35,21 @@ class CustomListenerSpec extends TestSpecBase {
 
     def setupSpec() {
         System.setProperty('spring.kafka.bootstrap-servers', embeddedKafka.getBrokersAsString());
-        System.setProperty('spring.cloud.stream.kafka.binder.zkNodes', embeddedKafka.getZookeeperConnectionString());
     }
 
     def 'Sample test'() {
         def testMessagePayload = "Test message"
         def message = MessageBuilder.withPayload(testMessagePayload).setHeader(KafkaHeaders.TOPIC, TOPIC_NAME).build()
+        def result = new BlockingVariable<Boolean>(5)
+        // Use this as a mock response and make it's response be to set the value in the BlockingVariable to true
+        service.handleMessage(_ as String) >> {
+            result.set(true)
+        }
 
         when: 'We put a message on the topic'
         template.send(message)
 
         then: 'the service should be called'
-        1 * service.handleMessage(_)
+        result.get()
     }
 }
--- src/test/groovy/com/example/TestSpecBase.groovy
+++ src/test/groovy/com/example/TestSpecBase.groovy
@@ -1,18 +1,21 @@
 package com.example
 
-import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.test.context.ActiveProfiles
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import spock.lang.Specification
 import spock.mock.DetachedMockFactory
 
-@ActiveProfiles('test')
 abstract class TestSpecBase extends Specification {
-    @TestConfiguration
+
+    // This has to be marked with a "real" @Configuration annotation in order for it to replace the standard config
+    // class and respect the @Primary bean.
+    @Configuration
     static class MockConfig {
         def detachedMockFactory = new DetachedMockFactory()
 
         @Bean
+        @Primary
         SimpleService mockService() {
             detachedMockFactory.Mock(SimpleService)
         }
